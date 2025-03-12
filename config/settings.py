38d211a7ea4fcf28571f5f0b78914af37d5460b2
Taha Ghazi <68954755+tahaghazi@users.noreply.github.com>
2025-03-12 13:50:35 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -248,10 +248,12 @@ AUTHENTICATION_BACKENDS = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    'https://becaitech.vercel.app',
 ]
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://becaitech.vercel.app',
 ]
 
 # ==============================================================================
